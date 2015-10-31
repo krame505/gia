36383719ebe5b6ef9bd1c8602a3d6a79f0c421a6
Lucas Kramer <krame505@umn.edu>
2015-10-31 15:54:45 -0500

    Fixed REPL bug with runtime error decls being re-evaluated in future decls

--- driver/Driver.sv
+++ driver/Driver.sv
@@ -21,17 +21,17 @@ IOVal<Integer> ::= args::[String] ioIn::IO parse::(ParseResult<cnc:Root>::=Strin
 
   return
     if null(args) || null(tail(args)) || null(tail(tail(args)))
-    then ioval(print("Error: missing arguments\nThis jar should not be run manually, only called via the gia script", ioIn), 5)
+    then ioval(print("Error: missing arguments\nThis jar should not be run manually, only called via the gia script", ioIn), 6)
     else if !result.parseSuccess
-    then ioval(print(result.parseErrors ++ "\n", cst.cnc:ioOut), 4)
+    then ioval(print(result.parseErrors ++ "\n", cst.cnc:ioOut), 5)
     else if !exprResult.parseSuccess
-    then ioval(print(exprResult.parseErrors ++ "\n", cst.cnc:ioOut), 3)
+    then ioval(print(exprResult.parseErrors ++ "\n", cst.cnc:ioOut), 4)
     else if !null(ast.errors)
-    then ioval(print(messagesToString(ast.errors) ++ "\n", cst.cnc:ioOut), if containsErrors(ast.errors, false) then 2 else 0)
+    then ioval(print(messagesToString(ast.errors) ++ "\n", cst.cnc:ioOut), if containsErrors(ast.errors, false) then 3 else 0)
     else if !null(ast.errors)
-    then ioval(print(messagesToString(ast.abs:evalErrors) ++ "\n", cst.cnc:ioOut), if containsErrors(ast.abs:evalErrors, false) then 1 else 0)
+    then ioval(print(messagesToString(ast.abs:evalErrors) ++ "\n", cst.cnc:ioOut), if containsErrors(ast.abs:evalErrors, false) then 2 else 0)
     else case ast.abs:evalRes of
-      abs:errorValue(_) -> ioval(print("Runtime errors:\n" ++ show(80, ast.abs:evalRes.abs:pp) ++ "\n", cst.cnc:ioOut), 0)
+      abs:errorValue(_) -> ioval(print("Runtime errors:\n" ++ show(80, ast.abs:evalRes.abs:pp) ++ "\n", cst.cnc:ioOut), 1)
     | _ -> ioval(print(show(80, ast.abs:evalRes.abs:pp) ++ " : " ++ show(80, ast.abs:evalResType.abs:pp) ++ "\n", cst.cnc:ioOut), 0)
     end;
 }
